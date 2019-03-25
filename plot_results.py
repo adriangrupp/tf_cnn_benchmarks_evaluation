25701612168f2b5f031360962ae7073bf5ea5b3a
ace <usmfz@student.kit.edu>
2019-03-25 15:47:11 +0100

    general improvements, titles and iterations added

--- plot_results.py
+++ plot_results.py
@@ -44,11 +44,10 @@ def getBenchParams(data):
 
 
 ### Histogramm Images/sec for 1,2,4 gpus, no dataset comaprison
-#TODO: coloured bars per gpu
-#TODO: Legend for numgpus
+#TODO: coloured bars per gpu + Legend
 #TODO: aesthetics
 #TODO: avoid empty plots (dic not in plotData) 
-def histImgSec(plotData, outDir):
+def histImgSec(plotData, metaData, outDir, std=False):
     """ Function to plot histogams that compare images/sec for 
         different amount of gpus and models. 1 Diagramm per dataset.
     """
@@ -70,17 +69,20 @@ def histImgSec(plotData, outDir):
         for m in models:    
             gpus = []
             examples_per_sec = [] 
+            stdevs = []
             for key, value in plotData.items():
                 if ds.lower() in key.lower() and m.lower() in key.lower():
                     gpus.append(value['benchmark_params'].get('num_gpus'))
                     examples_per_sec.append(value['averages'].get('average_examples_per_sec'))
+                    stdevs.append(value['stdevs'].get('average_examples_per_sec'))
 
             if len(examples_per_sec) == 0:
                 continue
             print(gpus, examples_per_sec)
 
             gpus_idx = np.array(gpus).argsort()    
-            examples_per_sec_sort = np.array(examples_per_sec)[gpus_idx]
+            examples_per_sec_sort = np.array(examples_per_sec)[gpus_idx].astype(int)
+            stdevs_sort = np.array(stdevs)[gpus_idx]
 
             plotData_x = list(np.arange(i, i+len(examples_per_sec)*0.25, 0.25))
             labelsPos.append(plotData_x[1]) #middle
@@ -89,9 +91,14 @@ def histImgSec(plotData, outDir):
             # Bar chart
             plt.bar(plotData_x, examples_per_sec_sort, width,
                     align='center', facecolor='g', alpha=0.5, edgecolor='w')
+            if (std):
+                #TODO aesthetics
+                plt.errorbar(plotData_x, examples_per_sec_sort,
+                        yerr=stdevs_sort, fmt='k.', ecolor='k', elinewidth=2)
             # Bar labels
             for j, v in enumerate(examples_per_sec_sort):
-                plt.text(i + j * width - width/2, v + 3, str("%.2f" % v), color='k')
+                plt.text(i + j * width - width/2, v + 3, str("%i" % v),
+                        color='k', fontsize='small')
             i+=1
 
         if len(labels) == 0:
@@ -100,7 +107,8 @@ def histImgSec(plotData, outDir):
         plt.xticks(labelsPos, labels)
         plt.xlabel('Model')
         plt.ylabel('Images/sec')
-        plt.title('Training: uDocker - 1,2 and 4 GPUs - %s Dataset' %ds)
+        plt.title('Training: %s - %s - 1,2 and 4 GPUs - %s Dataset'
+                %(metaData[0], metaData[1], ds)) #TODO nicer title
 
         filename = ds + '-Training.png'
         plt.savefig(outDir + '/' + filename)
@@ -108,10 +116,9 @@ def histImgSec(plotData, outDir):
     
 
 ## Histogram comparison real synthetic per num_gpu
-def histCompareRealSynth(plotData, outDir):
+def histCompareRealSynth(plotData, metaData, outDir):
 #TODO: Nicer bars
 #TODO: Nicer text
-#TODO: more generic?
     """ Function to compare histogams that compare images/sec for 
         different amount of gpus. Compares real to synth data. 
         Works only for imagenet - cifar10 has diferent models
@@ -140,22 +147,23 @@ def histCompareRealSynth(plotData, outDir):
 
         filename = m + '-SynthVsReal.png'
         outPath = outDir + '/' + filename            
+        meta = ['Synthetic', 'Real Data', metaData[0], metaData[2], metaData[1]]
         plotComparison(synt_ex_per_sec, synt_gpus, real_ex_per_sec,
-                real_gpus, outPath)
+                real_gpus, meta, outPath)
     return
 
 
 ## Histogram comparison between two sets of containers, systems, ...
-def histCompareTwoSets(plotData1, plotData2, outDir):
+def histCompareTwoSets(plotData1, plotData2, metaData, outDir):
     """ Function to plot histogams that compare images/sec for 
         two different sets. Compares per num_gpu.
         Requires: same dataset, same model.
         Currently only doing real datasets!
     """
-    #TODO: Legend names
     #TODO: Nicer bars
     #TODO: Nicer text
-    datasets = ['ImageNet-', 'CIFAR10']  #TODO better names
+    #TODO: synthetic data
+    datasets = ['Synthetic', 'ImageNet-', 'CIFAR10']  #TODO better names
     models = ['AlexNet', 'ResNet50', 'ResNet56']
 
     # One plot for each dataset and model to have all comparisons
@@ -180,15 +188,15 @@ def histCompareTwoSets(plotData1, plotData2, outDir):
             if len(ex_per_sec_1) == 0 or len(ex_per_sec_2) == 0:
                 continue
     
-            filename = ds + '-foo.png'
-            outPath = outDir + '/' + filename            
-            plotComparison(ex_per_sec_1, gpus_1, ex_per_sec_2, gpus_2, outDir)
+            filename = metaData[2] + '-' + ds + '-' + metaData[3] + '.png'
+            outPath = outDir + '/' + filename
+            metaData.append(ds)
+            plotComparison(ex_per_sec_1, gpus_1, ex_per_sec_2, gpus_2, metaData,
+                    outPath)
     return
 
 
-def plotComparison(ex_per_sec_1, gpus_1, ex_per_sec_2, gpus_2, outPath):
-    #TODO: Store plots
-
+def plotComparison(ex_per_sec_1, gpus_1, ex_per_sec_2, gpus_2, metaData, outPath):
     print(ex_per_sec_1, ex_per_sec_2)
 
     ax = plt.subplot2grid((1,1),(0,0))
@@ -222,22 +230,20 @@ def plotComparison(ex_per_sec_1, gpus_1, ex_per_sec_2, gpus_2, outPath):
     plt.xlabel('#GPUs')
     plt.ylabel('Images/sec')
 
-    plt.legend((p1[0], p2[0]), ('set 1', 'set 2')) #TODO fix names!
-    plt.title('Training: uDocker - %s - %s ' %('foo', 'bar'))
+    plt.legend((p1[0], p2[0]), (metaData[0], metaData[1]))
+    plt.title('%s vs %s - %s - %s ' %(metaData[0], metaData[1], metaData[2],
+        metaData[4])) #TODO maybe add epochs to title
     plt.savefig(outPath)
     return
 
 
-## Speedup
-
-## Sclaing
-def plotScaling(plotData, outDir):
+## Scaling
+def plotScaling(plotData, metaData, outDir):
     """ Plot the scaling for a dataset and several models for each num_gpu.
     Consider only images/sec. Includes also ideal scaling.
     """
-    #TODO: Store plots
     #TODO: nicer grid
-    #TODO: store
+    #TODO: avoid empty plots
     datasets = ['Synthetic', 'ImageNet-', 'CIFAR10']  #TODO better names
     models = ['AlexNet', 'ResNet50', 'ResNet56']
 
@@ -263,11 +269,11 @@ def plotScaling(plotData, outDir):
             gpus_sort = sorted(gpus)
             ex_per_sec_sort = np.array(ex_per_sec)[gpus_idx]
             
-
             # Ideal number of images is number of images for 1 gpu times num_gpus
             scaling = [x/ex_per_sec_sort[0] for x in ex_per_sec_sort]
             plt.plot(gpus_sort, scaling, label=m)
         
+
         # ideal scaling
         plt.plot([1,2,4], [1,2,4], label='Ideal')
 
@@ -276,9 +282,13 @@ def plotScaling(plotData, outDir):
         plt.ylabel('Scaling')
     
         plt.legend(loc=2, fontsize="small")
-        plt.title('Scaling: uDocker - %s dataset' %ds)
+        plt.title('Scaling: %s - %s - %s Dataset' %(metaData[0], metaData[1], ds))
         plt.grid()
-        plt.show()
+
+        filename = ds + '- scaling.png'
+        outPath = outDir + '/' + filename
+        plt.savefig(outPath)
+        plt.clf()
     return
 
 
@@ -289,8 +299,10 @@ def plotScaling(plotData, outDir):
 ## Evaluation examples/sec
 
 
+#-----------------------------------------------------------------
 if __name__ == "__main__":
     #TODO argv for selection of plot mode?
+    #TODO Verbose output for data being processed
 
     # Dir for benchmark data
     wd = os.getcwd()
@@ -310,45 +322,84 @@ if __name__ == "__main__":
             'fh2_udocker_5epoch',       'fh2_udocker_500batch',
             'fh2_singularity_5epoch',   'fh2_singularity_500batch']
 
-    #--------------------------------------------------
+    metaData = [['LSDF', 'Udocker', '5 epochs'],        ['LSDF', 'Udocker', '500 batches'],
+                ['LSDF', 'Singularity', '5 epochs'],    ['LSDF', 'Singularity', '500 batches'],
+                ['ForHLR2', 'Udocker', '5 epochs'],     ['ForHLR2', 'Udocker', '500 batches'],
+                ['ForHLR2', 'Singularity', '5 epochs'], ['ForHLR2', 'Singularity', '500 batches']]
+
+    #-----------------------------------------------------------------
     
-    # Plot bar charts for imgs/sec to compare models
-    # Per dataset, all settings
-    """for f in folders:
-        procFiles = os.path.join(resultDir, f)
-        print('Processing: ' + procFiles)
-        plotData = readFiles(procFiles)
-        if plotData:
-            outDir = os.path.join(plotDir, f)
-            if not os.path.exists(outDir):
-                os.makedirs(outDir)
-            histImgSec(plotData, outDir)"""
-   
-
-    # Plot comparison of synthetic, vs real data, 500 batches, udocker, lsdf
-    """for f in folders:
-        procFiles = os.path.join(resultDir, f)
-        print('Processing: ' + procFiles)
-        plotData = readFiles(procFiles)
-        if plotData:
-            outDir = os.path.join(plotDir, f)
-            if not os.path.exists(outDir):
-                os.makedirs(outDir)
-            histCompareRealSynth(plotData, outDir)"""
-
-    # Plot comparison between lsdf & fh2, udocker, 500 batches, real data
-    #TODO: iteration
-    procFiles1 = os.path.join(resultDir, folders[1])
-    plotData1 = readFiles(procFiles1)
-    procFiles2 = os.path.join(resultDir, folders[5])
-    plotData2 = readFiles(procFiles2)
-    outDir = os.path.join(compDir, 'lsdf_vs_fh2') #TODO avoid overwriting
-    histCompareTwoSets(plotData1, plotData2, outDir)
-
-    # Plot scaling 500 batches, udocker lsdf
-    #TODO: iteration
+    ## Debug ##        
     """procFiles = os.path.join(resultDir, folders[1])
+    print('Processing: ' + procFiles)
     plotData = readFiles(procFiles)
     outDir = os.path.join(plotDir, folders[1])
-    plotScaling(plotData, outDir)
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+    histImgSec(plotData, metaData[1], outDir)
     """
+
+    # Plot bar charts for imgs/sec per num_gpu to compare models
+    # Per dataset, all folders
+    for i,f in enumerate(folders):
+        procFiles = os.path.join(resultDir, f)
+        print('Processing: ' + procFiles)
+        plotData = readFiles(procFiles)
+        if plotData:
+            outDir = os.path.join(plotDir, f)
+            if not os.path.exists(outDir):
+                os.makedirs(outDir)
+            histImgSec(plotData, metaData[i], outDir)
+   
+    # Plot comparison of synthetic vs real data(imagenet), all folders
+    for i,f in enumerate(folders):
+        procFiles = os.path.join(resultDir, f)
+        print('Processing: ' + procFiles)
+        plotData = readFiles(procFiles)
+        if plotData:
+            outDir = os.path.join(plotDir, f)
+            if not os.path.exists(outDir):
+                os.makedirs(outDir)
+            histCompareRealSynth(plotData, metaData[i], outDir)
+
+    # Plot comparison between singularity & udocker, all machines, all data 
+    for i in [0,1,4,5]:
+        procFiles1 = os.path.join(resultDir, folders[i])
+        plotData1 = readFiles(procFiles1)
+        procFiles2 = os.path.join(resultDir, folders[i+2])
+        plotData2 = readFiles(procFiles2)
+        meta = [metaData[i][1], metaData[i+2][1], metaData[i][0],
+            metaData[i][2]]
+        if plotData1 and plotData2:
+            outDir = os.path.join(compDir, 'udocker_vs_singularity')
+            if not os.path.exists(outDir):
+                os.makedirs(outDir)
+            histCompareTwoSets(plotData1, plotData2, meta, outDir)
+
+    # Plot comparison between lsdf & fh2, all containers, all data
+    for i in range(4):
+        procFiles1 = os.path.join(resultDir, folders[i])
+        plotData1 = readFiles(procFiles1)
+        procFiles2 = os.path.join(resultDir, folders[i+4])
+        plotData2 = readFiles(procFiles2)
+        meta = [metaData[i][0], metaData[i+4][0], metaData[i][1],
+            metaData[i][2]]
+        if plotData1 and plotData2:
+            outDir = os.path.join(compDir, 'lsdf_vs_fh2')
+            if not os.path.exists(outDir):
+                os.makedirs(outDir)
+            histCompareTwoSets(plotData1, plotData2, meta, outDir)
+
+    # Plot scaling 500 batches, real + synth
+    #TODO
+
+    # Plot scaling single folder, all folders, real&synth extra
+    for i,f in enumerate(folders):
+        procFiles = os.path.join(resultDir, f)
+        plotData = readFiles(procFiles)
+        print('Processing: ' + procFiles)
+        if plotData:
+            outDir = os.path.join(plotDir, f)
+            if not os.path.exists(outDir):
+                os.makedirs(outDir)
+            plotScaling(plotData, metaData[i], outDir)
